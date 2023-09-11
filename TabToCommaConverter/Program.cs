7e25a8b5c5d806d6d1320038191996dde190e948
Vlad Coceban <coceban.vlad@hotmail.com>
2023-09-11 17:24:15 +0300

    add code

---
+++ TabToCommaConverter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+var culture = CultureInfo.InvariantCulture;
+
+using var reader = new StreamReader(args[0]);
+using var csvReader = new CsvReader(reader, new CsvConfiguration(culture)
+{
+    Delimiter = "\t"
+});
+
+await using var writer = new StreamWriter(args[1]);
+await using var csvWriter = new CsvWriter(writer, new CsvConfiguration(culture)
+{
+    Delimiter = ","
+});
+
+var sw = Stopwatch.StartNew();
+
+Console.WriteLine("Converting...");
+await csvWriter.WriteRecordsAsync(csvReader.GetRecordsAsync<dynamic>());
+
+sw.Stop();
+Console.WriteLine($"Done {sw.Elapsed}");
